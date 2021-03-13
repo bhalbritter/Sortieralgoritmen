9c1c360f08eb5f87eda421a8fb6540322c20511d
bhalbritter <63356794+bhalbritter@users.noreply.github.com>
2021-03-13 18:56:25 +0100

    Add files via upload
    Added Avl-Tree files

---
+++ AvlElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aufgabe3
+{
+     class AvlElement
+    {
+        public int height;
+        public int value;
+        public AvlElement left, right;
+        
+    }
+}
